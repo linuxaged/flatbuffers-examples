2b63d2ac5a4426850f67b4f779e0f590fda7b759
Tracy Ma <1991md@gmail.com>
2017-10-28 13:28:58 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include "flatbuffers/flatbuffers.h"
+#include "wall_generated.h"
+
+using namespace mj2::schema;
+
+int main()
+{
+	flatbuffers::FlatBufferBuilder fbb;
+
+	auto str = fbb.CreateString("GoGoGo!");
+	auto uid = fbb.CreateString("madi");
+	auto content = fbb.CreateString("我从远方来。。。");
+
+	auto c0 = CreateComment(fbb, 1, 999, uid, content);
+
+	std::vector<flatbuffers::Offset<Comment>> comment_offsets;
+	comment_offsets.push_back(c0);
+	auto vec = fbb.CreateVector(comment_offsets);
+
+	auto end = CreateWall(fbb, str, vec);
+
+	fbb.Finish(end);
+
+	FILE* bin = fopen("comment_wall.bin", "wb");
+	if (bin)
+	{
+		fwrite(fbb.GetBufferPointer(), sizeof(char), fbb.GetSize(), bin);
+		fclose(bin);
+	}
+
+	return 0;
+}
